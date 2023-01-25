8e62d28307eb553157611e23803d19cf9629a2b3
logog6 <logog1999@gmail.com>
2023-01-25 21:06:16 +0100

    Part 2
    Dodanie button'ow(relacja z ilością obrazów w folderze)/ scroller poziomów/tekstura w LevelButtor/przejście do scen po nacisnieciu/MainMenu skrypt

---
+++ CubeGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject levelButtonPrefab;
+    public GameObject levelButtonContainer;
+
+    private void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+        foreach(Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(levelButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(levelButtonContainer.transform,false);
+
+            string sceneName = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+        }
+    }
+
+    private void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
