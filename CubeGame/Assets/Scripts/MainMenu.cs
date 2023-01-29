672e7e146aced09f3c1ce1f574e7055e74e53dab
logog6 <logog1999@gmail.com>
2023-01-29 21:49:07 +0100

    Part 4
    Sklep/wybór skina ze sklepu/zmiana skina gracza

--- CubeGame/Assets/Scripts/MainMenu.cs
+++ CubeGame/Assets/Scripts/MainMenu.cs
@@ -8,13 +8,19 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject levelButtonPrefab;
     public GameObject levelButtonContainer;
+    public GameObject shopButtonPrefab;
+    public GameObject shopItemsContainer;
+
+    public Material playerMaterial;
 
     private Transform cameraTransform;
     private Transform cameraDesiredLookAt;
 
     private void Start()
     {
+        ChangePlayerSkin(0);
         cameraTransform = Camera.main.transform;
+
         Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
         foreach(Sprite thumbnail in thumbnails)
         {
@@ -24,6 +30,19 @@ public class MainMenu : MonoBehaviour
 
             string sceneName = thumbnail.name;
             container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+        }
+
+        int textureIndex = 0;
+        Sprite[] textures = Resources.LoadAll<Sprite>("Player");
+        foreach (Sprite texture in textures)
+        {
+            GameObject container = Instantiate(shopButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = texture;
+            container.transform.SetParent(shopItemsContainer.transform, false);
+
+            int index = textureIndex;
+            container.GetComponent<Button>().onClick.AddListener(() => ChangePlayerSkin(index));
+            textureIndex++;
         }
     }
 
@@ -44,5 +63,22 @@ public class MainMenu : MonoBehaviour
     {
         //Camera.main.transform.LookAt(menuTranform.position);
         cameraDesiredLookAt = menuTranform;
+    }
+
+    private void ChangePlayerSkin (int index)
+    {
+        float x = (index % 4) * 0.25f;
+        float y = (index / 4) * 0.25f;
+
+        if (y == 0.0f)
+            y = 0.75f;
+        else if (y == 0.25f)
+            y = 0.5f;
+        else if (y == 0.50f)
+            y = 0.25f;
+        else if (y == 0.75f)
+            y = 0.0f;
+
+        playerMaterial.SetTextureOffset("_MainTex", new Vector2(x, y));
     }
 }
