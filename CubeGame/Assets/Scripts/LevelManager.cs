9811aea603b7df07c22a73c71c8c5a76c08ba472
logog6 <logog1999@gmail.com>
2023-01-31 00:25:44 +0100

    Part 6
    Game UI w poziomach/przycisk pauzy/powrót do mainmenu z poziomu/dodanie Cuba oraz polatformy(początek tylko z rightbody)

---
+++ CubeGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
